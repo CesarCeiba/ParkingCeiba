ef992b7cbfcb3d205c801662f57a1b8c885b6c52
CEIBA\cesar.velasquez <cesar.velasquez@ceiba.com.co>
2018-01-31 17:09:24 -0500

    Terminando mejora de código e implementación de pruebas unitarias restantes V3

--- src/main/java/co/ceiba/parking/crypto/Cryptografy.java
+++ src/main/java/co/ceiba/parking/crypto/Cryptografy.java
@@ -12,22 +12,31 @@ import org.apache.tomcat.util.codec.binary.Base64;
 import co.ceiba.parking.exception.ParqueaderoException;
 
 public class Cryptografy {
+		
+	private static String codificacion = "utf-8";
+	private static String algoritmoCodificacion = "MD5";
+	private static String estandarEncriptado = "DESede";
+	private static String secretKey = "CeibaParking";
+	
+	private Cryptografy(){
+		
+	}
+	
 	public static String encriptar(String texto) throws ParqueaderoException {
 		
-        String secretKey = "CeibaParking";
         String base64EncryptedString = "";
  
         try {
  
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] digestOfPassword = md.digest(secretKey.getBytes("utf-8"));
+            MessageDigest md = MessageDigest.getInstance(algoritmoCodificacion);
+            byte[] digestOfPassword = md.digest(secretKey.getBytes(codificacion));
             byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
  
-            SecretKey key = new SecretKeySpec(keyBytes, "DESede");
-            Cipher cipher = Cipher.getInstance("DESede");
+            SecretKey key = new SecretKeySpec(keyBytes, estandarEncriptado);
+            Cipher cipher = Cipher.getInstance(estandarEncriptado);
             cipher.init(Cipher.ENCRYPT_MODE, key);
  
-            byte[] plainTextBytes = texto.getBytes("utf-8");
+            byte[] plainTextBytes = texto.getBytes(codificacion);
             byte[] buf = cipher.doFinal(plainTextBytes);
             byte[] base64Bytes = Base64.encodeBase64(buf);
             base64EncryptedString = new String(base64Bytes);
@@ -40,22 +49,21 @@ public class Cryptografy {
 	
 	public static String desencriptar(String textoEncriptado) throws ParqueaderoException {
 		 
-        String secretKey = "CeibaParking"; //llave para encriptar datos
         String base64EncryptedString = "";
  
         try {
-            byte[] message = Base64.decodeBase64(textoEncriptado.getBytes("utf-8"));
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] digestOfPassword = md.digest(secretKey.getBytes("utf-8"));
+            byte[] message = Base64.decodeBase64(textoEncriptado.getBytes(codificacion));
+            MessageDigest md = MessageDigest.getInstance(algoritmoCodificacion);
+            byte[] digestOfPassword = md.digest(secretKey.getBytes(codificacion));
             byte[] keyBytes = Arrays.copyOf(digestOfPassword, 24);
-            SecretKey key = new SecretKeySpec(keyBytes, "DESede");
+            SecretKey key = new SecretKeySpec(keyBytes, estandarEncriptado);
  
-            Cipher decipher = Cipher.getInstance("DESede");
+            Cipher decipher = Cipher.getInstance(estandarEncriptado);
             decipher.init(Cipher.DECRYPT_MODE, key);
  
             byte[] plainText = decipher.doFinal(message);
  
-            base64EncryptedString = new String(plainText, "UTF-8");
+            base64EncryptedString = new String(plainText, codificacion);
  
         } catch (Exception ex) {
         	throw new ParqueaderoException ("Ha ocurrido al encriptar la clave: "+ex.getMessage());
